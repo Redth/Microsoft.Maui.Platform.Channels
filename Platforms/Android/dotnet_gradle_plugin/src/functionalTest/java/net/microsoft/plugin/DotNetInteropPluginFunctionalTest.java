b011cdae15adbaac97461debc11bc61625670f97
redth <jondick@gmail.com>
2023-12-20 09:54:27 -0500

    Add import

--- Platforms/Android/dotnet_gradle_plugin/src/functionalTest/java/net/microsoft/plugin/DotNetInteropPluginFunctionalTest.java
+++ Platforms/Android/dotnet_gradle_plugin/src/functionalTest/java/net/microsoft/plugin/DotNetInteropPluginFunctionalTest.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertTrue;
 
